a84eb650cc1162aed6246503cc5c1d03b4af9e82
Ryan Soto <rsoto003@ucr.edu>
2019-05-27 23:00:29 -0700

    displaying weather data on screen on button click

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,17 +6,16 @@ class Form extends Component {
 
   <div className="row">
     <div className="col s6 push-s3">
-      <div className="card grey darken-2">
+      <div className="card grey darken-4">
         <div className="card-content white-text">
           <span className="card-title">type your location...</span>
           <form onSubmit={this.props.getWeather}>
-              <input type="text" name="city" placeholder="City"/>
-              <input type="text" name="country" placeholder="Country"/>
-              <button className="waves-effect   red darken-3 waves-light btn-large">Get Weather!</button>
+              <input type="text" name="city" placeholder="City" className="grey darken-4 white-text validate"/>
+              <input type="text" name="country" placeholder="Country" className="grey darken-4 white-text validate"/>
+              <button className="waves-effect red darken-3 waves-light btn-large">Get Weather!</button>
             </form>
+        </div>
       </div>
-    </div>
-      <button onClick={this.props.alertResults} className="waves-effect waves-light btn">Testing Props</button>
     </div>
   </div>
         );
