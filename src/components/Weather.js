1c3a2f2e7177803910c17e6d21429f9e3b4a6af6
Ryan Soto <rsoto003@ucr.edu>
2019-05-28 23:37:40 -0700

    weather data and weather placeholder headings now show only on button click

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 
 class Weather extends Component {
+
+//How to get weather information to only show when button is clicked?
+//
     render(){
         return (
-            
-                
-                <div className="row">
+             <div className="row">
                 <div className="col s6 push-s3">
                   <div className="card grey darken-4">
                     <div className="card-content white-text">
-                      <span className="card-title">Weather Details:</span>
-                      {this.props.temperature} <br />
-                      {this.props.city} <br />
-                      {this.props.country} <br />
+                    {this.props.city && this.props.country && <span className="card-title">Weather Details:</span>}
+                        { this.props.city && this.props.country && <p>City: {this.props.city}, {this.props.country}</p>}
+                        { this.props.temperature &&  <p>Temperature: {this.props.temperature}</p>}
+                        { this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+                        { this.props.description && <p>Conditions: {this.props.description}</p>}
                     </div>
                   </div>
                 </div>
